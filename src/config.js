656c06bec84230d0d70b5c6b05502474f634907b
Umer Gouri <65413356+Airdroprozana@users.noreply.github.com>
2025-01-08 10:06:32 +0530

    config.js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 class Config {
   constructor() {
-    this.baseURL = 'https://nodepay.org';
+    this.baseURL = 'https://app.nodepay.ai';
     this.ipCheckURL = 'https://ipinfo.io/json';
     this.pingURL = 'http://18.142.29.174/api/network/ping';
-    this.retryInterval = 30000;
+    this.retryInterval = 300;
     this.sessionURL = 'http://api.nodepay.ai/api/auth/session';
   }
 }
